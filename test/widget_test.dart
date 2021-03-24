88f66ff17a8ada0a6e2d315f171132e511041617
GHAlmeidaDev <gaabriel.hudson11@gmail.com>
2021-03-24 09:15:36 -0300

    Detector

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:facemaskdetection/main.dart';
+import 'package:detectordemascara/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
